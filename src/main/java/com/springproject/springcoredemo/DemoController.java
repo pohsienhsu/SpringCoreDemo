dc7a3084e212b0eccb177bf26f9cd047afbe3bfc
phsu40 <phsu40@gatech.edu>
2023-04-02 20:13:07 -0700

    Create DemoController rest controller

---
+++ src/main/java/com/springproject/springcoredemo/DemoController.java
@@ -0,0 +1,17 @@
+package com.springproject.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+}
