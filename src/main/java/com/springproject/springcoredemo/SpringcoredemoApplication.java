0a416c878d5fa56f391506e3bae5f6c6157049be
phsu40 <phsu40@gatech.edu>
2023-04-03 15:22:06 -0700

    Refactor Spring project and classes

--- src/main/java/com/springproject/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/springproject/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,11 @@ package com.springproject.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*@SpringBootApplication(
+		scanBasePackages = {"com.springproject.springcoredemo",
+							"com.springproject.util"}
+)
+ */
 @SpringBootApplication
 public class SpringcoredemoApplication {
 	public static void main(String[] args) {
