76cd097b4cd7b1accdfa0341cd27b4646172200f
phsu40 <phsu40@gatech.edu>
2023-04-03 17:58:22 -0700

    Update bean scopes to default and initialize post-construct & pre-destroy methods

--- src/main/java/com/springproject/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/springproject/springcoredemo/common/CricketCoach.java
@@ -1,11 +1,8 @@
 package com.springproject.springcoredemo.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
--- src/main/java/com/springproject/springcoredemo/rest/DemoController.java
+++ src/main/java/com/springproject/springcoredemo/rest/DemoController.java
@@ -1,6 +1,8 @@
 package com.springproject.springcoredemo.rest;
 
 import com.springproject.springcoredemo.common.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,25 +13,28 @@ public class DemoController {
 
     // define a private field for the dependency
     private Coach myCoach;
-    private Coach anotherCoach;
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(
-            @Qualifier("cricketCoach") Coach theCoach,
-            @Qualifier("cricketCoach") Coach theAnotherCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
-    }
-
-    @GetMapping("/check")
-    public String comparing() {
-        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
