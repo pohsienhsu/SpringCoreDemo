d91e2d6e1f6c031e71820b91b28a3af72fbf5527
phsu40 <phsu40@gatech.edu>
2023-04-03 17:48:48 -0700

    Update bean scopes from default to prototype

--- src/main/java/com/springproject/springcoredemo/rest/DemoController.java
+++ src/main/java/com/springproject/springcoredemo/rest/DemoController.java
@@ -11,16 +11,25 @@ public class DemoController {
 
     // define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach,
+            @Qualifier("cricketCoach") Coach theAnotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String comparing() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
